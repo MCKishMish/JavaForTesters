12c255eeb93766ebe21cb1774f0dff734c9f06fa
MCKishMish <pashabest032@gmail.com>
2021-07-21 23:55:56 +0300

    Class Point added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
@@ -9,10 +9,16 @@ public class MyFirstProg {
     hello("Pavel");
 
     //double l=5;
-    System.out.println("Площадь квадрата со стороной "+s.l+ " = "+area(s));
+    System.out.println("Площадь квадрата со стороной "+s.l+ " = "+s.area());
 
     Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b +  " = " +area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b +  " = " +r.area());
+
+    Point p1= new Point(0,5);
+    Point p2= new Point(0,0);
+
+    System.out.println("Расстояние между двумя точками с координатами "
+            + "(" + p1.x + ";" + p1.y + ") и (" + p2.x + ";" + p2.y + ") равно " + p1.distance(p2));
   }
 
   public static void hello(String somebody) {
@@ -25,5 +31,9 @@ public class MyFirstProg {
 
   public static double area (Rectangle r){
     return r.a*r.b;
+  }
+
+  public static double distance(Point p1, Point p2){
+    return Math.sqrt(Math.pow((p2.x- p1.x),2)+Math.pow((p2.y- p1.y),2));
   }
 }
