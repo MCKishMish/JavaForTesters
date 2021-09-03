bcdc7ea8545d3f9ca8a355eca831cff7a47abc37
MCKishMish <pashabest032@gmail.com>
2021-09-03 18:43:43 +0300

    Реализация поиска максимального id контакта с помощью лямбда выражений

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,19 +15,13 @@ public class ContactCreationTests extends TestBase{
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().goToHomePage();
     List<ContactData> before= app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("FirstName1", "LastName1", "5 S.Main Street, Englishtown, NJ 077261", "+1 (202) 986-1805!", "e-mail1@mail.ru1", null);
+    ContactData contact = new ContactData("FirstName1", "LastName1", "5 S.Main Street, Englishtown, NJ 077261", "+1 (202) 986-1805!", "e-mail1@mail.ru1", "test1");
     app.getContactHelper().createContact(contact);
     app.getNavigationHelper().goToHomePage();
     List<ContactData> after= app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(),before.size()+1);
 
-    int max=0;
-    for (ContactData c : after) {
-      if (c.getId()>max) {
-        max=c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
   }
